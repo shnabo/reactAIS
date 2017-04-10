20e709f144e5b47c67adc8b51496b5f08a1c1efe
Steve Villella <s.villella@live.com>
2017-04-10 13:00:10 -0400

    Rendering to page

---
+++ app/components/About.js
@@ -0,0 +1,9 @@
+var React = require('react');
+
+var About = React.createClass ({
+  render () {
+    return(<div> About Us </div>);
+  }
+});
+
+module.exports = About;
--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 var React = require('react');
 var ReactDOM = require ('react-dom');
+var App = require('./components/App');
 
 
 
@@ -24,5 +25,3 @@ ReactDOM.render(
   <App />,
   document.getElementById('app')
 );
-
-module.exports = App;
