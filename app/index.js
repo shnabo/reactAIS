77311cbb08e28d3aad690e398ad30671b3fd2b07
Steve Villella <s.villella@live.com>
2017-04-20 16:20:43 -0400

    Add Bootstrap dependencies via NPM and import in to index js

--- app/index.js
+++ app/index.js
@@ -3,6 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import MainNav from './components/MainNav.js';
 import App from './components/app';
+import { alert, buttons, jumbotron } from 'bootstrap-css'
 
 
 ReactDOM.render(<App />, document.getElementById('app'))
