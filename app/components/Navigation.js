c528d508c09cafd0da44808e124b04a31a3d3fba
Steve Villella <s.villella@live.com>
2017-04-24 14:57:16 -0400

    Working navigation without importing

--- app/components/Navigation.js
+++ app/components/Navigation.js
@@ -1,107 +1,30 @@
-import React from 'react'
-import {
-  BrowserRouter as Router,
-  Route,
-  Link
-} from 'react-router-dom'
+import React from 'react';
+ import { Link } from 'react-router';
 
-const Home = () => (
-  <div>
-    <h2>Home</h2>
-  </div>
-)
+ export default class Navigation extends React.Component {
+   render() {
+     return (
+       <nav className="Nav">
+         <div className="Nav__container">
+           <Link to="/" className="Nav__brand">
+             Hello!
+           </Link>
 
-
-const About = () => (
-  <div>
-    <h2>About</h2>
-  </div>
-)
-const Contact = () => (
-  <div>
-    <h2>Contact</h2>
-  </div>
-)
-
-const Topic = ({ match }) => (
-  <div>
-    <h3>{match.params.serviceId}</h3>
-  </div>
-)
-
-const Services = ({ match }) => (
-  <div>
-    <h2>Services</h2>
-    <ul>
-      <li>
-        <div>
-        <Link to={`${match.url}/insurance`}>
-          Insurance Investigation
-        </Link>
-        </div>
-      </li>
-      <li>
-        <Link to={`${match.url}/spousal`}>
-          Spousal Investigation
-        </Link>
-      </li>
-      <li>
-        <Link to={`${match.url}/backgroundchecks`}>
-          Background Checks
-        </Link>
-      </li>
-      <li>
-        <Link to={`${match.url}/lossprevention`}>
-          Loss Prevention
-        </Link>
-      </li>
-      <li>
-        <Link to={`${match.url}/internaldisputes`}>
-          Internal Dispute Investigation
-        </Link>
-      </li>
-      <li>
-        <Link to={`${match.url}/debugging`}>
-          Debugging - Commerciall & Residential
-        </Link>
-      </li>
-      <li>
-        <Link to={`${match.url}/litigationsupport`}>
-          Litigation Support
-        </Link>
-      </li>
-      <li>
-        <Link to={`${match.url}/processserving`}>
-          Process Serving
-        </Link>
-      </li>
-    </ul>
-
-    <Route path={`${match.url}/:serviceId`} component={Topic}/>
-    <Route exact path={match.url} render={() => (
-      <h3>Please select a topic.</h3>
-    )}/>
-  </div>
-)
-
-const Apple= () => (
-  <Router>
-    <div>
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/about">About</Link></li>
-        <li><Link to="/services">Services</Link></li>
-        <li><Link to="/contact">Contact</Link></li>
-      </ul>
-
-      <hr/>
-
-      <Route exact path="/" component={Home}/>
-      <Route path="/about" component={About}/>
-      <Route path="/services" component={Services}/>
-      <Route path="/contact" component={Contact}/>
-      <Route path="/services/insurance" component={InsuranceInvestigation}/>
-    </div>
-  </Router>
-)
-export default Navigation
+           <div className="Nav__right">
+             <ul className="Nav__item-wrapper">
+               <li className="Nav__item">
+                 <Link className="Nav__link" to="/about">About</Link>
+               </li>
+               <li className="Nav__item">
+                 <Link className="Nav__link" to="/services">Services</Link>
+               </li>
+               <li className="Nav__item">
+                 <Link className="Nav__link" to="/cotact">Contact</Link>
+               </li>
+             </ul>
+           </div>
+         </div>
+       </nav>
+     );
+   }
+ }
