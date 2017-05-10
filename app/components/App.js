ef6646f9725453623b4bb90d161add35dc052692
Steve Villella <s.villella@live.com>
2017-05-10 15:26:58 -0400

    Create Navigation component and replace in App js

--- app/components/App.js
+++ app/components/App.js
@@ -37,12 +37,7 @@ const App= () => (
 
               <div className="12u">
                 <h1> AIS Investigation Services </h1>
-                  <nav id="nav">
-                    <LinkContainer to="/"><a id="navigation">Home</a></LinkContainer>
-                    <LinkContainer to="/about"><a id="navigation">About Us</a></LinkContainer>
-                    <LinkContainer to="/services"><a id="navigation">Services</a></LinkContainer>
-                    <LinkContainer to="/contact"><a id="navigation">Contact</a></LinkContainer>
-                  </nav>
+                  <NavigationLinks />
 
 
               </div>
